f733ca4d9a24726f35c24b6d492bc2feab7e6497
BIRD UP <birdupthemusical@outlook.com>
2020-07-31 18:06:36 +0200

    Update messageinfo command

--- src/commands/ungrouped/messageinfo.ts
+++ src/commands/ungrouped/messageinfo.ts
@@ -1,12 +1,13 @@
 import { ICommand } from '../../classes/Command';
-import { DMChannel, Message, MessageEmbed, NewsChannel, TextChannel } from 'discord.js';
-import { createFooter } from '../../util/Style';
+import { Client, DMChannel, Message, MessageEmbed, NewsChannel, TextChannel } from 'discord.js';
+import { createFooter, sendErrorEmbed } from '../../util/Style';
 import { getUsage } from '../../util/CommandUtil';
 
 export const command: ICommand = {
     name: 'messageinfo',
     description: 'Shows info of a message',
     aliases: ['message', 'msg', 'msginfo'],
+    args: true,
 
     async execute(message, args) {
         if (args.length != 1) {
@@ -15,27 +16,35 @@ export const command: ICommand = {
 
         let fetchedMsg: Message = await parseMessage(message.channel, args[0]);
 
-        const embed: MessageEmbed = createFooter(message.client);
-
-        embed.setTitle('Message info');
-        embed.setDescription(fetchedMsg.content);
-        embed.setThumbnail(fetchedMsg.author.avatarURL());
-
-        embed.addField('Sent by', fetchedMsg.author);
-        embed.addField('Message ID', `\`${fetchedMsg.id}\``);
-        embed.addField('Created at', fetchedMsg.createdAt.toUTCString());
-        embed.addField('URL', fetchedMsg.url);
-
-        let editedAt: Date | null = fetchedMsg.editedAt;
-
-        if (editedAt) {
-            embed.addField('Edited at', editedAt.toUTCString());
+        if (!fetchedMsg) {
+            await sendErrorEmbed(message, `Could not find message ${args[0]}`);
         }
 
-        await message.channel.send(embed);
+        await sendEmbed(message.client, fetchedMsg);
     },
 };
 
+async function sendEmbed(client: Client, message: Message) {
+    const embed: MessageEmbed = createFooter(client);
+
+    embed.setTitle('Message info');
+    embed.setDescription(message.content);
+    embed.setThumbnail(message.author.avatarURL());
+
+    embed.addField('Sent by', message.author);
+    embed.addField('Message ID', `\`${message.id}\``);
+    embed.addField('Created at', message.createdAt.toUTCString());
+    embed.addField('URL', message.url);
+
+    if (message.editedAt) {
+        embed.addField('Last edited at', message.editedAt.toUTCString());
+    }
+
+    await message.channel.send(embed);
+}
+
 async function parseMessage(channel: TextChannel | DMChannel | NewsChannel, arg: string): Promise<Message> {
-    return await channel.messages.fetch(arg);
+    return await channel.messages.fetch(arg).catch(_ => {
+        return null;
+    });
 }
